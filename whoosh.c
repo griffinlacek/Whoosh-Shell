c27ba112a9406b422f4dfe605db5c62bfa623c5d
Griffin Lacek <griffinlacek@gmail.com>
2016-02-13 19:13:12 -0600

    Initial setup

---
+++ whoosh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	char command[128];
+	
+	while(1) {
+		printf("whoosh> ");
+		scanf("%s", command);
+		
+		printf("You entered %s\n", command);
+	}
+	
+	// process creation
+
+
+}
